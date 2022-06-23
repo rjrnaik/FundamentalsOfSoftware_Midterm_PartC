ac18fe88a8f5678fdf9997a819e9583d3b8c958d
rjrnaik <naikra@sheridancollege.ca>
2022-06-23 11:05:54 -0400

    MidTerm_PartC

--- src/status/Status.java
+++ src/status/Status.java
@@ -7,27 +7,37 @@ package status;
 
 import java.util.Scanner;
 
-/** This class takes String input from user 
- * and calls method statusDetail to print the 
- * status of User-Rejected,Pending,Processing and Approved.
- * if the input is not given in String, 
- * program will accept and produce wrong results. 
- * change the code to use enums which avoids String input 
- * then print the status details. 
- * @author srinivsi 
+/**
+ * This class takes String input from user and calls method statusDetail to
+ * print the status of User-Rejected,Pending,Processing and Approved. if the
+ * input is not given in String, program will accept and produce wrong results.
+ * change the code to use enums which avoids String input then print the status
+ * details.
+ *
+ * @author srinivsi
+ * @author Rajrishi Naik
  */
 public class Status {
 
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args)
-    {
-    Scanner in =new Scanner(System.in);
-    System.out.println("Enter the user status code (zero,one,two,three) in string");
-    String code = in.next();
-    StausUser t= new StausUser();
-    t.statusDetail(code); 
+
+
+    public static void main(String[] args) {
+//    Scanner in =new Scanner(System.in);
+//    System.out.println("Enter the user status code (zero,one,two,three) in string");
+//    String code = in.next();
+//    StausUser t= new StausUser();
+//    t.statusDetail(code); 
+//        StausUser su = new StausUser(StausUser.Code, StausUser.CodeStatus.);
+        StausUser[] statusArr = new StausUser[5];
+       for(int i=0; i<5; i++){
+           statusArr[i] = new StausUser(StausUser.Code.values()[i], StausUser.CodeStatus.values()[i]);
+       }
+        for(int j=0; j<5; j++){
+            System.out.println("For Code: " + statusArr[j].getCode() +  " - Status is: " + statusArr[j].getCodeStatus());
+        }
     }
-    
+
 }
--- src/status/StausUser.java
+++ src/status/StausUser.java
@@ -8,27 +8,72 @@ package status;
 /**
  * MidTerm-Exam
  * @author srinivsi
+ * * @author Rajrishi Naik
  */
 public class StausUser 
 {
-   public void statusDetail(String code)
-{
-switch(code.toUpperCase())
-        {
-        case "ZERO": System.out.println("REJECTED");
-        break;
-        case "ONE": System.out.println("PENDING");
-        break;
-        case "TWO":
-        System.out.println("PROCESSING");
-        break;
-        case "THREE": 
-            System.out.println("APPROVED");
-        break;
-        default:
-         System.out.println("NOT VALID CODE");
-        break;
-        }
-}
+//   public void statusDetail(String code)
+//{
+//switch(code.toUpperCase())
+//        {
+//        case "ZERO": System.out.println("REJECTED");
+//        break;
+//        case "ONE": System.out.println("PENDING");
+//        break;
+//        case "TWO":
+//        System.out.println("PROCESSING");
+//        break;
+//        case "THREE": 
+//            System.out.println("APPROVED");
+//        break;
+//        default:
+//         System.out.println("NOT VALID CODE");
+//        break;
+//        }
+//}
+    
+    public enum Code {
+        ZERO, ONE, TWO, THREE, OTHER
+    };
  
+    public enum CodeStatus{
+        REJECTED, PENDING, PROCESSING, APPROVED, NOTVALID
+    }
+    
+    private Code code;
+    private CodeStatus codeStatus;
+
+    public StausUser(Code code, CodeStatus codeStatus) {
+        this.code = code;
+        this.codeStatus = codeStatus;
+    }
+    /**
+     * @return the code
+     */
+    public Code getCode() {
+        return code;
+    }
+
+    /**
+     * @param code the code to set
+     */
+    public void setCode(Code code) {
+        this.code = code;
+    }
+
+    /**
+     * @return the codeStatus
+     */
+    public CodeStatus getCodeStatus() {
+        return codeStatus;
+    }
+
+    /**
+     * @param codeStatus the codeStatus to set
+     */
+    public void setCodeStatus(CodeStatus codeStatus) {
+        this.codeStatus = codeStatus;
+    }
+    
+    
 }
